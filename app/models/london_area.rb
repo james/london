f049b00a6b8515c7e3a143cd57976a2ca2041222
James Darling <james@abscond.org>
2024-07-23 22:37:04 +0100

    First sketch of the SQL for getting the areas at a lat/long

--- app/models/london_area.rb
+++ app/models/london_area.rb
@@ -1,2 +1,5 @@
 class LondonArea < ApplicationRecord
+  def self.find_by_latitude_and_longitude(latitude, longitude)
+    LondonArea.where("ST_Contains(geometry::geometry, ST_SetSRID(ST_Point(?, ?), 4326))", longitude, latitude)
+  end
 end
