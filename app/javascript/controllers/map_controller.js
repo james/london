59c6af8f3465fc0b24d9a0f40c6e23f91c6b0786
James Darling <james@abscond.org>
2024-12-30 20:33:36 +0000

    Map goes to selected postcode
    Pass the lat and long into the stimulus controller and use it to put the pin in the map in the correct place

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 import L from 'leaflet'
 export default class extends Controller {
     connect() {
-        var map = L.map(this.element).setView([51.505, -0.09], 13);
+        const latitude = this.element.dataset.mapLatitude;
+        const longitude = this.element.dataset.mapLongitude;
+
+        var map = L.map(this.element).setView([latitude, longitude], 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
         
-        L.marker([51.5, -0.09]).addTo(map)
+        L.marker([latitude, longitude]).addTo(map);
     }
 }
