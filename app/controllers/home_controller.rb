0e6347dce084a912f195e7483c3b7f87b548f7e0
James Darling <james@abscond.org>
2025-01-21 21:52:04 +0000

    grab the formatted version of the postcode from postcodes.io
    So instead of the user seeing e105ll, they'll see E10 5LL

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     if params[:postcode]
       response = Net::HTTP.get(URI("https://api.postcodes.io/postcodes/#{URI.encode_uri_component(params[:postcode])}"))
       result = JSON.parse(response)
+      params[:postcode] = result['result']['postcode']
 
       if result['status'] == 200
         @scorecard = Scorecard.new(result['result']['latitude'], result['result']['longitude'])
