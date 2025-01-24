912e767ba192f37b0ec36fa4b5ea23f5599f6245
James Darling <james@abscond.org>
2025-01-24 22:17:07 +0000

    Only format postcode is postcode found
    If we got an error from postcodes.io, this errored.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,9 @@ class HomeController < ApplicationController
     if params[:postcode]
       response = Net::HTTP.get(URI("https://api.postcodes.io/postcodes/#{URI.encode_uri_component(params[:postcode])}"))
       result = JSON.parse(response)
-      params[:postcode] = result['result']['postcode']
 
       if result['status'] == 200
+        params[:postcode] = result['result']['postcode']
         @scorecard = Scorecard.new(result['result']['latitude'], result['result']['longitude'])
       else
         @error = "Postcode not found"
