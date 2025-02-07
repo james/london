da230956540be51f3116a38e62cc44fc504e5f1f
James Darling <james@abscond.org>
2025-02-07 19:23:58 +0000

    Add tube stations and dotted lines to them on map
    I was adding polygons to the maps, but nearest tube stations aren't a containing polygon but a nearby point.
    
    Create a new Scorecard attribute for geopoint
    Add function for it in map controller
    refactor clearning map so both polygons and points can clear either or

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,16 +16,40 @@ export default class extends Controller {
             apiKey: "vPTKvn3quFUGwtEkShs8",
             style: "85105982-deff-46dc-879f-70b866a06391"
           }).addTo(this.map);
-        L.marker([latitude, longitude]).addTo(this.map);
+        this.userMarker = L.marker([latitude, longitude]).addTo(this.map);
     }
 
     showArea(event) {
-        if (this.currentGeojsonLayer) {
-            this.map.removeLayer(this.currentGeojsonLayer);
-        }
-
+        this.clearMap();
         this.currentGeojsonLayer = L.geoJSON(JSON.parse(event.currentTarget.dataset.geojson));
         this.currentGeojsonLayer.addTo(this.map);
         this.map.fitBounds(this.currentGeojsonLayer.getBounds());
+    }
+
+    showPoint(event) {
+        this.clearMap();
+        const [longitude, latitude] = JSON.parse(event.currentTarget.dataset.geopoint).coordinates;
+        this.placeMarker = L.marker([latitude, longitude]);
+        this.placeMarker.addTo(this.map);
+
+        const latlngs = [
+            [this.element.dataset.mapLatitude, this.element.dataset.mapLongitude],
+            [latitude, longitude]
+        ];
+        this.line = L.polyline(latlngs, { color: 'blue', dashArray: '5, 10' }).addTo(this.map);
+
+        this.map.fitBounds(this.line.getBounds());
+    }
+
+    clearMap() {
+        if (this.currentGeojsonLayer) {
+            this.map.removeLayer(this.currentGeojsonLayer);
+        }
+        if (this.placeMarker) {
+            this.map.removeLayer(this.placeMarker);
+        }
+        if (this.line) {
+            this.map.removeLayer(this.line);
+        }
     }
 }
--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -17,30 +17,30 @@ class Scorecard
     if distance_to_nearest_tube_station < 5000
       case nearest_tube_station.zone.to_i
       when 1
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 50)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 50)
       when 2
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 40)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 40)
       when 3
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 30)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 30)
       when 4
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 20)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 20)
       when 5
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 10)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 10)
       when 6
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 5)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 5)
       when 7
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 4)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 4)
       when 8
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 3)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 3)
       when 9
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 2)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 2)
       when 10
-        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, points: 1)
+        scores << Score.new(template: 'nearest_tube', name: nearest_tube_station.name, zone: nearest_tube_station.zone.to_i, geo_point: nearest_tube_station.lonlat, points: 1)
       end
     end
 
     if distance_to_nearest_night_tube_station < 2500
-      scores << Score.new(template: 'night_tube', name: nearest_night_tube_station.name, points: 20)
+      scores << Score.new(template: 'night_tube', name: nearest_night_tube_station.name, geo_point: nearest_night_tube_station.lonlat, points: 20)
     end
   end
 
@@ -74,12 +74,13 @@ class Scorecard
 end
 
 class Score
-  attr_accessor :name, :template, :points, :geojson, :zone
+  attr_accessor :name, :template, :points, :geojson, :zone, :geo_point
   def initialize(args = {})
     @name = args[:name]
     @template = args[:template]
     @points = args[:points]
     @geojson = args[:geojson]
     @zone = args[:zone]
+    @geo_point = args[:geo_point]
   end
 end
