5adf4924a8d1689ed8f667bc4dc280b4b33e8a7f
James Darling <james@abscond.org>
2025-12-31 11:57:58 +0000

    Update lat long for scorecard spec
    The model spec for scorecard and hte request spec were using similar, but slightly different lat longs, which was causing me confision. I've made them the same.

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Scorecard, type: :model do
   describe "Leyton" do
-    let(:latitude) { 51.5650 }
-    let(:longitude) { -0.0300 }
+    let(:latitude) { 51.56185 }
+    let(:longitude) { -0.013446 }
     let(:scorecard) { Scorecard.new(latitude, longitude) }
 
     it "returns Leyton Midland station as the nearest station" do
-      leyton_midland_station = TubeStation.find_by(name: 'Leyton Midland Road')
-      expect(scorecard.nearest_tube_station).to eq(leyton_midland_station)
+      leyton_station = TubeStation.find_by(name: 'Leyton')
+      expect(scorecard.nearest_tube_station).to eq(leyton_station)
     end
 
     it "returns Leyton station as the nearest night station" do
@@ -17,26 +17,26 @@ RSpec.describe Scorecard, type: :model do
     end
 
     it "returns Here East North as nearest cycle dock" do
-      cycle_dock = CycleDock.find_by(name: 'Here East North, Queen Elizabeth Olympic Park')
+      cycle_dock = CycleDock.find_by(name: 'Lee Valley VeloPark, Queen Elizabeth Olympic Park')
       expect(scorecard.nearest_cycle_dock).to eq(cycle_dock)
     end
 
     it "returns 1881 for distance to nearest cycle dock" do
-      expect(scorecard.distance_to_nearest_cycle_dock).to eq(1881)
+      expect(scorecard.distance_to_nearest_cycle_dock).to eq(1397)
     end
 
     it "returns a ptal value" do
       expect(scorecard.ptal_value).to be_a(PtalValue)
     end
 
-    it "returns 56% as percentage of maximum score" do
+    it "returns 52% as percentage of maximum score" do
       expect(scorecard.percentage_of_max_score).to eq(56)
     end
   end
 
   describe "#max_score" do
     it "returns the maximum score" do
-      expect(Scorecard.max_score).to eq(240)
+      expect(Scorecard.max_score).to eq(260)
     end
   end
 
