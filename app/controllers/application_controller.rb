b132f1580b1ecf538367d8f87113b6aedae27136
James Darling <james@abscond.org>
2026-01-03 11:55:46 +0000

    Remove HTTP basic authentication from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::Base
-	before_action :http_basic_auth
-
-	private
-
-	def http_basic_auth
-		return if request.path == '/up'
-		username = ENV['BASIC_AUTH_USERNAME']
-		password = ENV['BASIC_AUTH_PASSWORD']
-
-		return unless username.present? && password.present?
-
-		authenticate_or_request_with_http_basic('Restricted') do |u, p|
-			u == username && p == password
-		end
-	end
 end
